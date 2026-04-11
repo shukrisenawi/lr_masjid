4c70e51fafb80ef332419c6fb5f246f5872ee0aa
shukrisenawi <shukrisenawi@gmail.com>
2026-04-12 00:36:17 +0800

    Buang fungsi delete akaun pada profil

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,6 @@ Route::get('/dashboard', DashboardController::class)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('members', MemberController::class)->except('show');
     Route::resource('committee-members', CommitteeMemberController::class)->except('show');
